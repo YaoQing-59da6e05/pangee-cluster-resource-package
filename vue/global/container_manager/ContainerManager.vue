22da30c3d3c58249cf530fe2ab7194d0c0704d2d
zhaoyucong <zhaoyucong@chinamobile.com>
2026-03-09 10:00:41 +0800

    cri_containerd_insecure_registries

--- vue/global/container_manager/ContainerManager.vue
+++ vue/global/container_manager/ContainerManager.vue
@@ -2,23 +2,25 @@
 en:
   cm_label: ContainerManager
   cm_description: Container Manager
-  cri_docker_root_size_place_holder: docker root dir size
+  cri_containerd_insecure_registries_place_holder: e.g. http://10.99.0.8:5000
 zh:
   cm_label: 容器引擎
   cm_description: 设置集群范围内所有节点使用的容器引擎相关参数
-  cri_docker_root_size_place_holder: docker 数据盘大小
+  cri_containerd_insecure_registries_place_holder: 例如：http://10.99.0.8:5000
 </i18n>
 
 <template>
   <ConfigSection v-model:enabled="enabled" :label="t('cm_label')" :description="t('cm_description')" disabled
     anti-freeze>
-    <ContainerManagerRegistry v-model="modelValue.all.children.target.vars.cri_containerd_insecure_registries">
-    </ContainerManagerRegistry>
+    <EditArray v-model="modelValue.all.children.target.vars.cri_containerd_insecure_registries" :prop="prop + '.cri_containerd_insecure_registries'" label="cri_containerd_insecure_registries" :item-rules="registryRules">
+      <template #editItem="scope">
+        <el-input v-model.trim="modelValue.all.children.target.vars.cri_containerd_insecure_registries[scope.index]" :placeholder="t('cri_containerd_insecure_registries_place_holder')"></el-input>
+      </template>
+    </EditArray>
   </ConfigSection>
 </template>
 
 <script lang="ts" setup>
-import ContainerManagerRegistry from './ContainerManagerRegistry.vue';
 import { inject, ref } from "vue";
 
 const t = inject("t");
@@ -32,5 +34,19 @@ defineProps<{
 }>()
 
 const prop = 'all.children.target.vars'
+
+const registryRules = [
+  { 
+    required: true, 
+    type: 'string', 
+    message: '字段不能为空', 
+    trigger: 'blur' 
+  },
+  {
+    pattern: /^https?:\/\/[a-zA-Z0-9][a-zA-Z0-9\-\.]*[a-zA-Z0-9](:[0-9]+)?$/, 
+    message: '必须以 http:// 或 https:// 开头，包含有效的域名或IP地址，可包含端口号', 
+    trigger: 'blur' 
+  }
+]
 
 </script>
